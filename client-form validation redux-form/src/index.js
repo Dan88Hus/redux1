87fcbde3ac15e0723dc7b17cc1d54996784a9760
Huseyin Daniel Ozdogan <huseyinozdogan@gmail.com>
2021-02-13 11:35:17 +0300

    form validation with redux-form at create new.js

---
+++ client-form validation redux-form/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose} from 'redux';
+
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './components/App';
+import reducers from './reducers';
+
+import reportWebVitals from './reportWebVitals';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers,
+  composeEnhancers(applyMiddleware()));
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
